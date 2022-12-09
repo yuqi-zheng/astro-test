282cae27868cea7b378b48b823b0afe34f216b45
yuqi-zheng <81061878+yuqi-zheng@users.noreply.github.com>
2022-12-09 16:43:17 +0800

    added loading icon/text (#1)
    * main test 2
    
    * Loading

---
+++ src/components/Loading.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Jellyfish } from 'svelte-loading-spinners';
+</script>
+
+<div id="loader">
+    <Jellyfish size="60" color="#77CCFF" unit="px" duration="2s"/>
+    <h1>DEVELOPMENT IN PROGRESS</h1>
+</div>
+
+<style>
+    #loader {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    h1 {
+        font-family: 'Archivo Black', sans-serif;
+    }
+</style>
