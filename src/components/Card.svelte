8ab107eb3ab0ee7eaa902d00ce2c3648d9d3576e
yuqi-zheng <yuqizheng02@gmail.com>
2023-01-05 01:02:05 +0800

    added navbar and footer to layout

---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let text;
+    export let icon;
+
+</script>
+
+<article class="card">
+    <div class="container">
+        <div class="header">
+        <h4>Design.</h4>
+        <img src={icon} alt = "palette" style="width:43.98px;height:43.98px;">
+        </div>
+        <div class="text">
+        <p>{text}</p>
+    </div>
+  </div>
+  <div class="bottom">
+    <p class="p2">Take a look at our gallery!</p>
+    <img src="arrow.svg" alt = "arrow" style="left: 3.96%; right: 3.96%; top: 29.29%; bottom: 29.29%;">
+  </div>
+  </article><!-- /card-one -->
+
