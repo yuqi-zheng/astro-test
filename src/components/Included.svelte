046e2d1fc0ff3e589ccf9f1697e62387fc1e267c
yuqi-zheng <yuqizheng02@gmail.com>
2023-01-06 10:15:15 +0800

    html of included panel

---
+++ src/components/Included.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div>
+    <div class="row"><div class="header"><img src="server.svg" alt="server icon"><h3>Hosting Fees</h3></div><div class="text">All hosting and security (SSL) certificate costs</div></div>
+    <div class="row"><div class="header"><img src="write.svg" alt="writing icon"><h3>Copywriting</h3></div><div class="text">Custom copywriting with SEO, including competitor analysis and keyword research</div></div>
+    <div class="row"><div class="header"><img src="draw.svg" alt="drawing icon"><h3>Graphic Design</h3></div><div class="text">A hand-drawn logo, as well as custom graphics in the design.</div></div>
+    <div class="row"><div class="header"><img src="graph.svg" alt="graph icon"><h3>Analytics</h3></div><div class="text">Website integration with Google Analytics, so you'll be up to date with all the stats.</div></div>
+    <div class="row"><div class="header"><img src="changes.svg" alt="cycle icon"><h3>Free Changes</h3></div><div class="text">2 hours of free bonus development time every month for any website alterations you want to make</div></div>
+    <div class="row"><div class="header"><img src="support.svg" alt="support agent icon"><h3>Support</h3></div><div class="text">Talk to real people, based in Melbourne</div></div>
+</div>
+
+<style>
+
+</style>
