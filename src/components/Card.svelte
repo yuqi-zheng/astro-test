3230964e34d09fef6fd2a52e0d79f4f66b0241f5
yuqi-zheng <yuqizheng02@gmail.com>
2023-01-06 13:47:04 +0800

    integrated content panels in home page

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,8 +1,9 @@
 <script>
+    import '../styles/global.css'
+    
     export let text;
     export let icon;
     export let header;
-    export let arrow;
     export let second;
 
 </script>
@@ -19,27 +20,62 @@
   </div>
   <div class="bottom">
     <p>{second}</p>
-    <img src={arrow} alt = "arrow" style="left: 3.96%; right: 3.96%; top: 29.29%; bottom: 29.29%;">
+    <img src='arrow.svg' alt = "arrow" style="left: 3.96%; right: 3.96%; top: 29.29%; bottom: 29.29%;">
   </div>
 </article>
 
 
 <style>
   .card {
-    /* Add shadows to create the "card" effect */
     box-sizing: border-box;
     box-shadow: 0px 0px 6px 2px rgba(48, 52, 56, 0.25);
+    height: 275px;
     width: 32%;
-    height: 90%;
-    background: #F7F5F3;
+    background-color: #F7F5F3;
     border: 3px solid #9BA6AE;
-    /* Low Shadow */
     border-radius: 10px;
-    padding: 30px 36px 36px;
+    padding: 18px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    align-items: flex-start;
+    font-family: 'Outfit', sans-serif;
   }
+
+  .container {
+    display: flex;
+    gap: 18px;
+    flex-direction: column;
+    align-items: flex-start;
+    overflow: hidden;
+}
+
+h4 {
+  font-size: 32px;
+  font-weight: 400;
+} 
+
+.text {
+  font-size: 18px;
+  font-weight: 300;
+  color: #161718;
+}
+
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.bottom {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-weight: 500;
+  font-size: 16px;
+  gap: 18px;
+  align-self: stretch;
+}
+
 </style>
 
--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -2,89 +2,40 @@
   import Card from './Card.svelte';
 </script>
 
-<section class="cards">
-  <Card header='Design.' text= 'No templates - we custom design your website from the ground up based on your needs.
-  You give us as much detail as you want and we will do the rest.' icon = 'palette.svg' second = 'Take a look at our gallery!' arrow = 'arrow.svg'/>
-  <Card header='Develop.' text= 'We hand-code each line to maximise performance. Lambda websites are built in Svelte for optimal
-  pagespeed - which will bump up your ranking in Google searches.' icon = 'keyboard.svg' second = 'Site speed and Google rankings' arrow = 'arrow.svg'/>
-  <Card header='Maintain.' text= 'We take care of any changes you want to make, and perform routine maintenance to keep your website
-  running optimally.' icon = "home_repair_service.svg" second = 'Small-business-friendly pricing' arrow = 'arrow.svg'/>
-</section>
+<div>
+  <h2><u>Everything</u> you need with no big unfront cost</h2>
+  <section class="cards">
+    <Card header='Design.' text= 'No templates - we custom design your website from the ground up based on your needs.
+    You give us as much detail as you want and we will do the rest.' icon = 'palette.svg' second = 'Take a look at our gallery!'/>
+    <Card header='Develop.' text= 'We hand-code each line to maximise performance. Lambda websites are optimised for
+    minimal loading times - which will bump up your ranking in Google searches.' icon = 'keyboard.svg' second = 'Site speed and Google rankings'/>
+    <Card header='Maintain.' text= 'We take care of any changes you want to make, and perform routine maintenance to keep your website
+    running optimally.' icon = "home_repair_service.svg" second = 'Small-business-friendly pricing'/>
+  </section>
+</div>
 
-<head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Outfit">
 <style>
   
-* {
-  margin: 0px;
-  padding: 0px;
-}
-
-
-.container {
-  display: flex;
-  gap: 18px;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  height: 198px;
-  overflow: hidden;
-}
-
-h4 {
-  font-family: "Outfit";
-  font-size: 24px;
-  font-weight: 500;
-  line-height: 60px;
-} 
-
-.text {
-  font-family: "Outfit";
-  font-style: normal;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 30px;
-  color: #161718;
-}
-
-.header {
-  width: 100%;
-  height: 60px;
+  h2 {
+    font-family: 'Outfit', sans-serif;
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 24px;
+  }
+.cards {
+  box-sizing: border-box;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 0px;
-}
-
-.cards {
-  box-sizing: border-box;
-/* Auto layout */
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 36px;
+  padding: 24px;
   gap: 10px;
   width: 100%;
-  height: 468px;
-  background: #5043BA;
+  background-color: #5043BA;
   border: 2px solid #6756DE;
   box-shadow: 0px 0px 24px 4px rgba(48, 52, 56, 0.25);
-  border-radius: 15px;
-}
-
-.bottom {
-  font-family: "Outfit";
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 30px;
-  gap: 18px;
-  align-self: stretch;
+  border-radius: 10px;
 }
 
 </style>
-</head>
 
